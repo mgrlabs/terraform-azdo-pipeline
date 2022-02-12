5e2144b1ba4facd81fa312e57968e73290efe0d5
Mark <mark@mgrlabs.com>
2022-02-12 11:47:49 +1100

    template testing

---
+++ environments/dev/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.1.0" # Pinned to a terraform version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.93.1" # Pinned to a provider version
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = 623aabe5-748d-4b15-be89-3119e247a9fd
+}
+
+# locals {
+#   tags = merge(var.tags, local.module_tags)
+#   module_tags = {
+#     "Module" = basename(abspath(path.module))
+#   }
+# }
+
+data "azurerm_client_config" "current" {}
+
+resource "random_password" "module" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
