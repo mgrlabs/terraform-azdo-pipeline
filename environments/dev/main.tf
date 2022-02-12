3c2679616bac6da18c18f93b5a3ea418dd06ef57
Mark <mark@mgrlabs.com>
2022-02-12 18:47:21 +1100

    template testing

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -26,5 +26,5 @@ data "azurerm_client_config" "current" {}
 resource "random_password" "module" {
   length           = 16
   special          = true
-  override_special        = "_%@"
+  override_special = "_%@"
 }
